3c668508eb484b5c94399183fe3d2c181cdd5bb5
EduardoAngelo2803 <leam@cesar.school>
2022-09-29 00:07:53 -0300

    Update Entradas e Pipe

--- shell.c
+++ shell.c
@@ -4,17 +4,17 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 #define MAX_ARGS 80
 #define BUFFER 1024
 #define READ_END
 #define WRITE_END
-// Global variable for store the arguments
+
 int flags;
 pid_t pidPipe[2];
 char *argv[MAX_ARGS / 2 + 1];
 char *auxargs[MAX_ARGS / 2 + 1];
-int verEOF = 0;
-// Global variable to store user command
+int verEOF, verEOF2 = 0;
 char input_user[BUFFER];
 char guideInput[BUFFER];
 int countHistory = 0;
@@ -26,34 +26,14 @@ int statusexit = 0;
 int countI = 0;
 int count = 0;
 int i;
-int g = 0;
 
-void checkChar(str) {
+void removeSpaces(char **str) {
 
-    int exclamationCheck, ptvgla, file;
-
-    if (strchr(str, '|') != NULL)
-    {
-        exclamationCheck = 1;
-
-    }else if (strchr(str, ";") != NULL) {
-
-        ptvgla = 1;
-
-    }else if (strchr(str, ">") != NULL) {
-
-        file = 1;
-    }
-}
-
-void removeSpaces(char **str)
-{
     while (**str == ' ')
         (*str)++;
 }
 
-void get_input()
-{
+void get_input() {
     // get command from user
     if(flagChoose == 1) {
 
@@ -81,23 +61,22 @@ void get_input()
         strcpy(guideInput, input_user);
        
     }
-    if (strcmp(input_user, "!!") == 0)
-    {
-        if (countGlobal == 0)
-        {
+    if (strcmp(input_user, "!!") == 0) {
+
+        if (countGlobal == 0) {
+
             printf("No commands\n");
         }
         flagH = 1;
     }
     
-    if (strcmp(input_user, "exit") == 0)
-    {
+    if (strcmp(input_user, "exit") == 0) {
+
         printf("Ending our Shell application, GoodBye!\n");
         statusexit = 1;
         exit(0);
-    }
-        // remove trailing newline       
-    }
+    } 
+}
 
 void separatorArgv (char *argv[BUFFER]) {
 
@@ -115,8 +94,7 @@ void separatorArgv (char *argv[BUFFER]) {
     auxargs[count] = NULL;
 }
 
-void separatorInput(char input_user[BUFFER])
-{
+void separatorInput(char input_user[BUFFER]) {
     char *delimit = ";|>";
     char *verDelim= strdup(input_user);
     char *separatorStr;
@@ -124,27 +102,26 @@ void separatorInput(char input_user[BUFFER])
 
     separatorStr = strtok(input_user, delimit);
 
-    if (verDelim[separatorStr - input_user + strlen(input_user)] == ';')
-    {
+    if (verDelim[separatorStr - input_user + strlen(input_user)] == ';') {
 
         flags = 1;
-    }
-    else if (verDelim[separatorStr - input_user + strlen(input_user)] == '|') {
+
+    } else if (verDelim[separatorStr - input_user + strlen(input_user)] == '|') {
 
         flags = 2;
 
-    }else if (verDelim[separatorStr - input_user + strlen(input_user)] == '>') {
+    } else if (verDelim[separatorStr - input_user + strlen(input_user)] == '>') {
 
         flags = 3;
     }
    
-    while (separatorStr != NULL)
-    {
+    while (separatorStr != NULL) {
+
         argv[i] = separatorStr;
         removeSpaces(&argv[i]);
         i++;
         separatorStr = strtok(NULL, delimit);
-        }
+    }
 
     free(verDelim);
 }
@@ -152,30 +129,37 @@ void separatorInput(char input_user[BUFFER])
 int chooseMode(char input_initial[30]) {
 
     printf("Choose mode for run Shell! style parallel or style sequential, '!!' to execute last command, and exit for endding!\n");
-    fgets(input_initial, 30, stdin);
+    verEOF2 = fgets(input_initial, 30, stdin);
+    input_initial[strlen(input_initial) - 1] = '\0';
+    if (input_initial == EOF) {
+
+        flagChoose = 3;
+        exit(0);
+    }
 
     if(input_initial == NULL) {
         flagChoose = 3;
         exit(0);
-        }
-    if (input_initial[strlen(input_initial) - 1] == '\n')
-    {
+    }
+    if (input_initial[strlen(input_initial) - 1] == '\n') {
+
         input_initial[strlen(input_initial) - 1] = '\0';
     }
-    if (strcmp(input_initial, "style sequential") == 0)
-    {
+    if (strcmp(input_initial, "style sequential") == 0) {
+
         return 1;
     }
     else if (strcmp(input_initial, "style parallel") == 0) 
     {
         return 2;
     }
-    else if (strcmp(input_initial, "exit") == 0)
-    {
+    else if (strcmp(input_initial, "exit") == 0) {
+
         printf("Ending our Shell application, GoodBye!\n");
         return 3;
         
-    }else if (strcmp(input_initial, "!!") == 0) {
+    }
+    else if (strcmp(input_initial, "!!") == 0) {
 
         printf("No commands\n");
         return 4;
@@ -190,7 +174,7 @@ void execPid() {
 
         //separatorInput(input_user);
 
-    }else if(flagH == 1) {
+    } else if(flagH == 1) {
 
         separatorInput(guideInput);
         flagH = 0;
@@ -201,40 +185,38 @@ void execPid() {
         }
     }
 
-    for (int j = 0; j < i; j++)
-    {
-        pid_t cpid[i];
+    for (int j = 0; j < i; j++) {
 
+        pid_t cpid[i];
         cpid[j] = fork();
 
         separatorArgv(&argv);
 
-        if (cpid[j] < 0 )
-        {
+        if (cpid[j] < 0 ) {
+
             printf("Fork failed!\n");
         }
-        else if (cpid[j] == 0)
-        {
+        else if (cpid[j] == 0) {
+
             execvp(auxargs[0], auxargs);
         }
-        else
-        {
+        else {
+
             printf("Command: %s\n", auxargs[0]);
             wait(NULL);
         }     
         //Clear array
-        for (int k = 0; k < i; k++)
-        {
-            auxargs[k] = NULL;
-            
+        for (int k = 0; k < i; k++) {
+
+            auxargs[k] = NULL;     
         }
         countI++;
         count = 0;   
     }  
 }
 
-void runInSequential()
-{
+void runInSequential() {
+
     if(statusexit != 1) {
 
         while(1) {
@@ -251,20 +233,13 @@ void runInSequential()
                 countI = 0;
                 execPid();
     
-            }
-
-            else if (flagH != 1)
-            {
-
+            }else if (flagH != 1) {
                 for (int l = 0; l < countI; l++)
                 {
 
                     argv[l] = NULL;
                 }
-                //memset(argv, 0, MAX_ARGS / 2 + 1);
                 countI = 0;
-                // i = 0;
-                // count = 0;
             }
 
             if(strcmp("exit", input_user) == 0) {
@@ -272,14 +247,18 @@ void runInSequential()
                 break;
             }
             separatorInput(input_user);
-            if(flags == 1) {
+            if(flags == 1 || flags == 0) {
 
                 execPid();   
 
             }else if(flags == 2) {
 
                 Pipe();
-            }  
+
+            }else if(flags == 3) {
+
+                readFile();
+            }
         }
     }
 }
@@ -352,8 +331,8 @@ void runInParallel() {
     }
 }
 
-void execPipe()
-{
+void execPipe() {
+
     char write_msg[BUFFER];
     char read_msg[BUFFER];
 
@@ -361,69 +340,101 @@ void execPipe()
     separatorArgv(&argv);
 
     execvp(auxargs[0], auxargs);
-    
-    
+  
 }
 
 void Pipe() {
 
     int fd[2];
-    pidPipe[g] = fork();
+    pidPipe[0] = fork();
    
-    if (pipe(fd) < 0)
-    {
+    if (pidPipe < 0) {
+
         printf("Error!\n");
-    }
-    else if (pidPipe[g] == 0)
-    {
-        close(fd[0]);
+
+    }else if (pidPipe[0] == 0) {
+        
         dup2(fd[1], STDOUT_FILENO);
-        execPipe();
+        close(fd[1]);
+        close(fd[0]);
+        separatorArgv(&argv);
+
+        execvp(auxargs[0], auxargs);
+
+    }else{
+
+        wait(NULL);
     }
-    for (int k = 0; k < i; k++)
-    {
+
+    for (int k = 0; k < i; k++) {
+
         auxargs[k] = NULL;
     }
     countI++;
     count = 0;
-    g++;
-    pidPipe[g] = fork();
+    pidPipe[1] = fork();
 
-    if (pidPipe[g] < 0)
-    {
+    if (pidPipe[1] < 0) {
+
         printf("Error!\n");
-    }
-    else if (pidPipe[g] == 0)
-    {
-        close(fd[1]);
-        dup2(fd[0], STDIN_FILENO);
+
+    } else if (pidPipe[1] == 0) {
         
-        execPipe();
+        dup2(fd[0], STDIN_FILENO);
+        separatorArgv(&argv);
+        close(fd[1]);
+        execvp(auxargs[0], auxargs);
+
+    }else{
+    
+        wait(NULL);
     }
 
     close(fd[0]);
     close(fd[1]);
+}
+
+void readFile() {
+
+    FILE *file;
+
+    int fd = open("newfile.txt", O_CREAT | O_WRONLY, 0777);
+
+    separatorArgv(&argv);
+
+    pid_t pid1;
+
+    pid1 = fork();
+    close(STDOUT_FILENO);   
+    dup2(fd, STDOUT_FILENO);
+    close(fd);
+    if(pid1 < 0) {
+
+        printf("Error!\n");
+
+    }else if(pid1 == 0) {
+
+        execvp(auxargs[0], auxargs);
     
-    while(g >= 0) {
+    }else {
 
         wait(NULL);
-        g--;
-    }
+    } 
 }
 
 int main()
 {
     int should_run = 1;
-    
+
     flagChoose = chooseMode(input_initial);
 
     while (should_run)
     {
         //Trying resolve 'ctrl d' to end the file
-        if(verEOF == EOF) {
+        if(verEOF == EOF || verEOF2 == EOF) {
 
             should_run = 0;
-            break;
+            exit(0);
         }
         if(should_run == 0) {
             break;
